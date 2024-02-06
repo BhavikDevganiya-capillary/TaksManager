5b8fffbf68a58e94ea8c7308cd6121d857e3ef28
BhavikDevganiya-capillary <bhavik.devganiya@capillarytech.com>
2024-02-06 16:43:04 +0530

    Added Slide page TDD test in test file

--- src/App.test.js
+++ src/App.test.js
@@ -6,4 +6,15 @@ describe("write test cases for task manager", () => {
     //check wether button is there or not
     //Check if redirection is working when you are click on link or not
   });
+  test("Slide Form testcases", () => {
+    //Check wether input box is there when page loads
+    //check wether add slide button is there or not
+    //check if input box is empty then its creating slide or not
+    //Enter slide name using fireEvent and check wether its there in input box or not
+    //click button using fireEvent check is button is clicked or not
+    //check wether new slide is added to the ui or not
+    //Check wether slides name is similar as you entered or not
+    //check if there is inputbox and button to create task
+    //check if inputbox is empty and you click on add task button does it create task or not
+  });
 });
