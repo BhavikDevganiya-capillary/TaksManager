617768e4558302c3b1b462272179e399588dad48
BhavikDevganiya-capillary <bhavik.devganiya@capillarytech.com>
2024-02-06 16:40:33 +0530

    Added navbar TDD test in test file

---
+++ src/App.test.js
@@ -0,0 +1,9 @@
+describe("write test cases for task manager", () => {
+  test("ui test cases", () => {
+    //Check wether navbar loaded or not
+    //check wether header is there or not
+    //check wether all links are there or not to navigate
+    //check wether button is there or not
+    //Check if redirection is working when you are click on link or not
+  });
+});
