9895ec4c5b95023a5f67f406f40fb94c7202d0fe
BhavikDevganiya-capillary <bhavik.devganiya@capillarytech.com>
2024-02-06 17:15:17 +0530

    Added test file with some of TDD UT test list

---
+++ src/App.test.js
@@ -0,0 +1,9 @@
+describe("write test cases for task manager", () => {
+  test("ui test cases", () => {
+    //Check whether navbar loaded or not
+    //check whether header is there or not
+    //check whether all links are there or not to navigate
+    //check whether button is there or not
+    //Check if redirection is working when you are click on link or not
+  });
+});
