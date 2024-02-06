60cd40e9be335cddd78bc81eb4742baafbd9c7d9
BhavikDevganiya-capillary <bhavik.devganiya@capillarytech.com>
2024-02-06 16:53:28 +0530

    Added product page testcase

--- src/App.test.js
+++ src/App.test.js
@@ -1,20 +1,34 @@
 describe("write test cases for task manager", () => {
   test("ui test cases", () => {
-    //Check wether navbar loaded or not
-    //check wether header is there or not
-    //check wether all links are there or not to navigate
-    //check wether button is there or not
+    //Check whether navbar loaded or not
+    //check whether header is there or not
+    //check whether all links are there or not to navigate
+    //check whether button is there or not
     //Check if redirection is working when you are click on link or not
   });
-  test("Slide Form testcases", () => {
-    //Check wether input box is there when page loads
-    //check wether add slide button is there or not
+  test("Slide testcases", () => {
+    //Check whether input box is there when page loads
+    //check whether add slide button is there or not
     //check if input box is empty then its creating slide or not
-    //Enter slide name using fireEvent and check wether its there in input box or not
+    //Enter slide name using fireEvent and check whether its there in input box or not
     //click button using fireEvent check is button is clicked or not
-    //check wether new slide is added to the ui or not
-    //Check wether slides name is similar as you entered or not
+    //check whether new slide is added to the ui or not
+    //Check whether slides name is similar as you entered or not
     //check if there is inputbox and button to create task
     //check if inputbox is empty and you click on add task button does it create task or not
+    //check if slide delete button is there and its working or not
+    //check if task is there is not then add one
+    //check if task delete and edit button is there or not
+    //click on edit button and check if input box is there or not and its value is same as shown
+    //try to rewrite value of task
+    //check whether there is check button is there or not
+    //click on that button and check is task value is changes from old to new value
+    //check whether task delete button is working or not click on delete slide and check whether task is deleted or not
+  });
+  test("Product page testcases", () => {
+    //check whether loading is shown or not
+    //check if product page is there or not
+    //check whether  it has table in page or not
+    //check whether table contains data inside it or not
   });
 });
