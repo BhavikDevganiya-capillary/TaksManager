95a2512a19de346a90c98a4cf8ac06291519d74e
BhavikDevganiya-capillary <bhavik.devganiya@capillarytech.com>
2024-02-07 10:36:53 +0530

    Added testcases for product details

---
+++ src/App.test.js
@@ -0,0 +1,8 @@
+describe("write test cases for task manager", () => {
+  test("Product page testcases", () => {
+    //check whether loading is shown or not
+    //check if product page is there or not
+    //check whether  it has table in page or not
+    //check whether table contains data inside it or not
+  });
+});
