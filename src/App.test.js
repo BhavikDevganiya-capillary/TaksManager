3ef608380dcc612b9031d8b37b0c010e0c63e7de
BhavikDevganiya-capillary <bhavik.devganiya@capillarytech.com>
2024-02-07 10:11:07 +0530

    Added testcases for Slides

--- src/App.test.js
+++ src/App.test.js
@@ -6,4 +6,23 @@ describe("write test cases for task manager", () => {
     //check whether button is there or not
     //Check if redirection is working when you are click on link or not
   });
+  test("Slide testcases", () => {
+    //Check whether input box is there when page loads
+    //check whether add slide button is there or not
+    //check if input box is empty then its creating slide or not
+    //Enter slide name using fireEvent and check whether its there in input box or not
+    //click button using fireEvent check is button is clicked or not
+    //check whether new slide is added to the ui or not
+    //Check whether slides name is similar as you entered or not
+    //check if there is inputbox and button to create task
+    //check if inputbox is empty and you click on add task button does it create task or not
+    //check if slide delete button is there and its working or not
+    //check if task is there is not then add one
+    //check if task delete and edit button is there or not
+    //click on edit button and check if input box is there or not and its value is same as shown
+    //try to rewrite value of task
+    //check whether there is check button is there or not
+    //click on that button and check is task value is changes from old to new value
+    //check whether task delete button is working or not click on delete slide and check whether task is deleted or not
+  });
 });
